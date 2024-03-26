32185045d622767f348dea5f6f1afdb32788ef04
mirabor <mirawithyu@gmail.com>
2024-03-26 09:29:13 -0400

    hide screens

--- src/app/pages/pages.ts
+++ src/app/pages/pages.ts
@@ -1,11 +1,11 @@
 export const pages = [
-  { index: 0, name: 'overview.md', route: '/overview', visible: true },
-  { index: 1, name: 'skills.md', route: '/skills', visible: true },
-  { index: 2, name: 'experience.md', route: '/experience', visible: true },
+  { index: 0, name: 'overview.md', route: '/overview', visible: false },
+  { index: 1, name: 'skills.md', route: '/skills', visible: false },
+  { index: 2, name: 'experience.md', route: '/experience', visible: false },
   { index: 3, name: 'education.md', route: '/education', visible: true },
-  { index: 4, name: 'projects.md', route: '/projects', visible: true },
+  { index: 4, name: 'projects.md', route: '/projects', visible: false },
   { index: 5, name: 'coursework.md', route: '/coursework', visible: true },
-  { index: 6, name: 'accomplishments.md', route: '/accomplishments', visible: true },  
+  { index: 6, name: 'accomplishments.md', route: '/accomplishments', visible: false },  
   { index: 7, name: 'current_picks.md', route: '/current_picks', visible: true },
   { index: 8, name: 'docs.md', route: '/docs', visible: false },  
 ];
